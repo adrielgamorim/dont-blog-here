ffa5db5f4543e81c79474462c2863e9c92bfb7b7
Adriel Amorim <agimenescomercial@gmail.com>
2022-08-08 12:38:31 -0300

    HashRouter problem solved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { useState } from 'react';
 import { auth } from './firebase-config'
 import { signOut } from 'firebase/auth';
